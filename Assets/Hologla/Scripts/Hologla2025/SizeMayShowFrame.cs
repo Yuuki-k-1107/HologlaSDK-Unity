dbcb19ef0822f0f837150d1942bcbf1d98254af5
Yukun1107 <yukun1107k@softbank.ne.jp>
2025-02-27 14:33:12 +0900

    追加：現在セレクトしている設定を表示できるようにした

--- Assets/Hologla/Scripts/Hologla2025/SizeMayShowFrame.cs
+++ Assets/Hologla/Scripts/Hologla2025/SizeMayShowFrame.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 
 /// <summary>
 /// 画面サイズが一致しているときに外枠を表示するスクリプト。
+/// BlockMenuList5にそのままアタッチする。
 /// </summary>
-public class SizeMayShowFrame : MayShowSelectFrame
+public class SizeMayShowFrame : MonoBehaviour
 {
+    [SerializeField] private GameObject[] frameObjects;
     void Update()
+    {
+    }
+
+    private void OnEnable()
     {
         FrameCheck();
     }
 
-    public override void FrameCheck()
+    public void FrameCheck()
     {
-        // UserSettings.viewSizeのインデックスはサイズ番号より1小さい点に注意。
-        // たとえばSize4の場合はsizeValueに3をセットする
-        if (null != selectFrameObject && (int)UserSettings.viewSize == value)
+        var currentViewSize = (int)UserSettings.viewSize;
+        for(int i=0; i<frameObjects.Length; i++)
         {
-            selectFrameObject.SetActive(true);
+            if(null == frameObjects[i]) continue;
+            frameObjects[i].SetActive(false);
+            if(currentViewSize == i)
+            {
+                frameObjects[i].SetActive(true);
+            }
         }
-        else
-        {
-            selectFrameObject.SetActive(false);
-        }
+        Debug.Log($"viewsize = {UserSettings.viewSize}");
+        //if (null != selectFrameObject)
+        //{
+        //    selectFrameObject.SetActive(false);
+        //    // UserSettings.viewSizeのインデックスはサイズ番号より1小さい点に注意。
+        //    // たとえばSize4の場合はsizeValueに3をセットする
+        //    if ((int)UserSettings.viewSize == value)
+        //    {
+        //        selectFrameObject.SetActive(true);
+        //    }
+        //}
     }
 }
