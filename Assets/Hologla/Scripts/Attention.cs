fbf232a7e3007817c2ab36e68e768b96b6e36d0a
Yukun1107 <yukun1107k@softbank.ne.jp>
2025-03-08 21:29:03 +0900

    追加 : 起動時のAttention画面

---
+++ Assets/Hologla/Scripts/Attention.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Attention : MonoBehaviour
+{
+    //シーン遷移までの秒数を指定
+    [SerializeField] private float _waitTimer = 10.0f;
+    [SerializeField] private string _nextScene;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //指定秒数を超えたら、メインシーンへ遷移
+        _waitTimer -= Time.deltaTime;
+        if (_waitTimer <= 0f)
+        {
+            SceneManager.LoadScene(_nextScene);
+        }
+
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_nextScene);
+    }
+
+
+}
